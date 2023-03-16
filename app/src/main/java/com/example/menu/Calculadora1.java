57d4c77704629e79f2ee00bb2a93b5f2bbab298f
Balliz <pietrotile@gmail.com>
2023-03-16 17:40:54 -0300

    Calculadora com envio de resultado para outra tela Calculadora que volta da tela de resultado para editar valores ou mudar a operação

--- app/src/main/java/com/example/menu/Calculadora1.java
+++ app/src/main/java/com/example/menu/Calculadora1.java
@@ -19,9 +19,51 @@ public class Calculadora1 extends AppCompatActivity {
     EditText etCalc1Num2;
     RadioGroup rgOperacoes1;
     int sinal = 0;
-
+    Double num1 = 0.0;
+    Double num2 = 0.0;
     TextView tvResultado1;
     Double resultado = 0.0;
+
+    public void Calcular() {
+        String texto1 = etCalc1Num1.getText().toString();
+        if (!texto1.isEmpty()) {
+            try {
+                num1 = Double.parseDouble(texto1);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+        String texto2 = etCalc1Num2.getText().toString();
+        if (!texto2.isEmpty()) {
+            try {
+                num2 = Double.parseDouble(texto2);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+        switch (sinal) {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                try {
+                    resultado = num1 / num2;
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+                break;
+        }
+        tvResultado1.setText("Resultado: " + resultado);
+        etCalc1Num1.setText(resultado.toString());
+        etCalc1Num2.setText("");
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +76,31 @@ public class Calculadora1 extends AppCompatActivity {
         btnResultado1 = findViewById(R.id.btnResultado1);
         tvResultado1 = findViewById(R.id.tvResultado1);
         btnMenu1 = findViewById(R.id.btnMenu1);
+        try {
+            Bundle extras = getIntent().getExtras();
+            Double num01 = extras.getDouble("Num01");
+            Double num02 = extras.getDouble("Num02");
+            int sinal1 = extras.getInt("Sinal");
+            etCalc1Num1.setText(num01.toString());
+            etCalc1Num2.setText(num02.toString());
+            switch (sinal1) {
+                case 1:
+                    rgOperacoes1.check(R.id.rbAdicao1);
+                    break;
+                case 2:
+                    rgOperacoes1.check(R.id.rbSubtracao1);
+                    break;
+                case 3:
+                    rgOperacoes1.check(R.id.rbMultiplicacao1);
+                    break;
+                case 4:
+                    rgOperacoes1.check(R.id.rbDivisao1);
+                    break;
+            }
+        }catch (Exception e)
+        {
+        }
+
         btnMenu1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,7 +111,7 @@ public class Calculadora1 extends AppCompatActivity {
         rgOperacoes1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                switch (i){
+                switch (i) {
                     case R.id.rbAdicao1:
                         sinal = 1;
                         break;
@@ -60,51 +127,22 @@ public class Calculadora1 extends AppCompatActivity {
                 }
             }
         });
+
         btnCalcular1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Double num1 = 0.0;
-                Double num2 = 0.0;
-                String texto1 =etCalc1Num1.getText().toString();
-                if(!texto1.isEmpty()) {
-                    try {
-                        num1 = Double.parseDouble(texto1);
-                    } catch (Exception e1) {
-                        e1.printStackTrace();
-                    }
-                }
-                String texto2 =etCalc1Num2.getText().toString();
-                if(!texto2.isEmpty()) {
-                    try {
-                        num2 = Double.parseDouble(texto2);
-                    } catch (Exception e1) {
-                        e1.printStackTrace();
-                    }
-                }
-                switch(sinal){
-                    case 1:
-                        resultado = num1 + num2;
-                    break;
-                    case 2:
-                        resultado = num1 - num2;
-                    break;
-                    case 3:
-                        resultado = num1 * num2;
-                    break;
-                    case 4:
-                        try {
-                            resultado = num1 / num2;
-                        }catch (Exception e2){
-                            e2.printStackTrace();
-                        }
-                    break;
-                }
-                tvResultado1.setText("Resultado: "+resultado);
-                etCalc1Num1.setText(resultado.toString());
-                etCalc1Num2.setText("");
-
-                Intent i = new Intent(Calculadora1.this, Resultado.class);
-                i.putExtra("Resultado",resultado);
+                Calcular();
+            }
+        });
+        btnResultado1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calcular();
+                Intent i = new Intent(Calculadora1.this, Resultado.class);;
+                i.putExtra("Resultado", resultado);
+                i.putExtra("Num1", num1);
+                i.putExtra("Num2", num2);
+                i.putExtra("Sinal", sinal);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/menu/Resultado.java
+++ app/src/main/java/com/example/menu/Resultado.java
@@ -2,16 +2,39 @@ package com.example.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
 
+    Button btnEditar1;
+    TextView tvResultado2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
-
         Bundle extras = getIntent().getExtras();
-            Double resultado = extras.getDouble("Resultado");
+        Double resultado = extras.getDouble("Resultado");
+        Double num1 = extras.getDouble("Num1");
+        Double num2 = extras.getDouble("Num2");
+        int sinal = extras.getInt("Sinal");
+        tvResultado2 = findViewById(R.id.tvResultado2);
+        tvResultado2.setText("Resultado: "+resultado);
+
+        btnEditar1 = findViewById(R.id.btnEditar1);
+        btnEditar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i5 = new Intent(Resultado.this, Calculadora1.class);
+                i5.putExtra("Resultado", resultado);
+                i5.putExtra("Num01", num1);
+                i5.putExtra("Num02", num2);
+                i5.putExtra("Sinal",sinal);
+                startActivity(i5);
+            }
+        });
     }
 }
